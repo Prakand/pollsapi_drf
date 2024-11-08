89376d0b00c56225f2bcaac4769dcee58dba757e
Prakand Mishra <prakandmishra5100@gmail.com>
2024-11-08 16:46:15 +0530

    made_basic_view

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
 from .views import polls_detail,polls_list
+from .apiviews import PollList,PollDetail,LoginView,RegisterView,LogoutView
 
 
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("login/",LoginView.as_view(),name = "login"),
+    path('register/',RegisterView.as_view(),name='register_user'),
+    path('logout/',LogoutView.as_view(),name="logout_user"),
 ]
